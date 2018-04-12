2c38dfc5c0f0366f30ac5c70d7f2dbdadc461d46
vagrant <vagrant@homestead>
2018-04-12 13:11:02 +0000

    Updated datsbase configureation

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    // 'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -30,7 +30,10 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
+$db_config = get_db_config();
 
+return [
+    'default' => $db_config['connection'],
     'connections' => [
 
         'sqlite' => [
