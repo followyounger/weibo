659c3dfb5f654b39f9ae6024f7f59bac515fd015
vagrant <vagrant@homestead>
2018-04-13 08:05:41 +0000

    Finish user edit,update,index and destory actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -6,7 +6,15 @@ use Illuminate\Http\Request;
 use Auth;
 class SessionsController extends Controller
 {
-    //
+
+    public function __construct(){
+        $this->middleware('guest',[
+            'only' => ['create']
+        ]);
+    }
+
+
+
     public function create(){
         return view('sessions.create');
     }
@@ -18,17 +26,15 @@ class SessionsController extends Controller
 
         ]);
         if (Auth::attempt($credentials, $request->has('remember'))) {
-
-            # code...
             session()->flash('success','欢迎回来');
-            return redirect()->route('users.show',[Auth::user()]);
+            return redirect()->intended(route('users.show', [Auth::user()]));
         }else{
             session()->flash('danger','很抱歉，你的邮箱和密码不匹配');
             return redirect()->back();
         }
     }
 
-    public function destory(){
+    public function destroy(){
         Auth::logout();
         session()->flash('success','您已经成功退出！');
         return redirect('login');
